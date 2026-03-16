298b4c45974bb4cac98c96df3dfa72a389e76c25
axeljavier-alvarez <axjavier4alvarez44@gmail.com>
2026-03-16 11:01:01 -0600

    pages

--- app/Models/DesarrolloSocial/BitacoraConstancia.php
+++ app/Models/DesarrolloSocial/BitacoraConstancia.php
@@ -2,9 +2,11 @@
 
 namespace App\Models\DesarrolloSocial;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BitacoraConstancia extends Model
 {
-    //
+    use HasFactory;
+    
 }
--- routes/desarrollo-social.php
+++ routes/desarrollo-social.php
@@ -8,4 +8,10 @@ Route::prefix('constancias-residencias')->group(function () {
     Route::view('solicitud','desarrollo-social.constancias-residencias.solicitud')
         ->name('constancias-residencias.solicitud');
 
+
+        Route::view('dashboard','desarrollo-social.constancias-residencias.solicitud')
+        ->middleware(['can:page.view.constancias-residencias.dashboard'])
+        ->name('constancias-residencias.dashboard');
+
+
 });
